ee281ab8a8fe545341276082f6876e82f62fffd7
Peter Lindblom <peter.lindblom.79@gmail.com>
2025-11-16 21:04:32 +0100

    Simple binary clock for the console.
    Added some tests

--- src/test/java/se/plweb/binaryClockCli/BinaryClockTest.java
+++ src/test/java/se/plweb/binaryClockCli/BinaryClockTest.java
@@ -109,4 +109,78 @@ class BinaryClockTest {
         assertArrayEquals(expectedTime, actualTime);
     }
 
+    @org.junit.jupiter.api.Test
+    void binaryTimeFrom7() {
+        // Sunday 16 November 2025 07:07:07
+        boolean[][] actualTime = BinaryClock.binaryTimeFrom(1763276827000L);
+        boolean[][] expectedTime = {
+                {false, false, false, false},
+                {false, true, true, true},
+                {false, false, false, false},
+                {false, true, true, true},
+                {false, false, false, false},
+                {false, true, true, true},
+        };
+        assertArrayEquals(expectedTime, actualTime);
+    }
+
+    @org.junit.jupiter.api.Test
+    void binaryTimeFrom8() {
+        // Sunday 16 November 2025 08:08:08
+        boolean[][] actualTime = BinaryClock.binaryTimeFrom(1763280488000L);
+        boolean[][] expectedTime = {
+                {false, false, false, false},
+                {true, false, false, false},
+                {false, false, false, false},
+                {true, false, false, false},
+                {false, false, false, false},
+                {true, false, false, false},
+        };
+        assertArrayEquals(expectedTime, actualTime);
+    }
+
+    @org.junit.jupiter.api.Test
+    void binaryTimeFrom9() {
+        // Sunday 16 November 2025 09:09:09
+        boolean[][] actualTime = BinaryClock.binaryTimeFrom(1763284149000L);
+        boolean[][] expectedTime = {
+                {false, false, false, false},
+                {true, false, false, true},
+                {false, false, false, false},
+                {true, false, false, true},
+                {false, false, false, false},
+                {true, false, false, true},
+        };
+        assertArrayEquals(expectedTime, actualTime);
+    }
+
+    @org.junit.jupiter.api.Test
+    void binaryTimeFrom10() {
+        // Sunday 16 November 2025 10:10:10
+        boolean[][] actualTime = BinaryClock.binaryTimeFrom(1763287810000L);
+        boolean[][] expectedTime = {
+                {false, false, false, true},
+                {false, false, false, false},
+                {false, false, false, true},
+                {false, false, false, false},
+                {false, false, false, true},
+                {false, false, false, false},
+        };
+        assertArrayEquals(expectedTime, actualTime);
+    }
+
+    @org.junit.jupiter.api.Test
+    void binaryTimeFrom11() {
+        // Sunday 16 November 2025 11:11:11
+        boolean[][] actualTime = BinaryClock.binaryTimeFrom(1763291471000L);
+        boolean[][] expectedTime = {
+                {false, false, false, true},
+                {false, false, false, true},
+                {false, false, false, true},
+                {false, false, false, true},
+                {false, false, false, true},
+                {false, false, false, true},
+        };
+        assertArrayEquals(expectedTime, actualTime);
+    }
 }
